d7903804c18f5c08dac98425202ec604756ec342
henrywoody <github@henrywoody.com>
2018-10-01 12:02:06 -0700

    adds evolve method and updates evaluation method

--- tests/test-evaluation.py
+++ tests/test-evaluation.py
@@ -1,27 +1,27 @@
 import unittest
 from unittest.mock import Mock, call
 
-from holland.evolution.evaluation import run_evaluation
+from holland.evolution.evaluation import evaluate_fitness
 
 
-class RunEvaluationTest(unittest.TestCase):
+class EvaluateFitnessTest(unittest.TestCase):
     def test_calls_evaluate_on_each_element_of_gene_pool(self):
-        """run_evaluation calls evaluate on each individual in gene_pool"""
-        evaluate = Mock()
+        """evaluate_fitness calls fitness_function on each individual in gene_pool"""
+        fitness_function = Mock()
         gene_pool = [1, 2, 3, 4, 5, 6]
 
-        run_evaluation(evaluate, gene_pool)
+        evaluate_fitness(gene_pool, fitness_function)
 
         expected_calls = [call(genome) for genome in gene_pool]
-        evaluate.assert_has_calls(expected_calls)
+        fitness_function.assert_has_calls(expected_calls)
 
     def test_returns_tuples_of_score_and_genome(self):
-        """run_evaluation pairs each genome with the score received from calling evaluate on that genome and returns a list of all tuples"""
+        """evaluate_fitness pairs each genome with the score received from calling fitness_function on that genome and returns a list of all tuples"""
         scores = [10, 20, 30, 40, 50, 60]
-        evaluate = Mock(side_effect=scores)
+        fitness_function = Mock(side_effect=scores)
         gene_pool = [1, 2, 3, 4, 5, 6]
 
-        results = run_evaluation(evaluate, gene_pool)
+        results = evaluate_fitness(gene_pool, fitness_function)
 
         expected_results = list(zip(scores, gene_pool))
         self.assertListEqual(results, expected_results)
