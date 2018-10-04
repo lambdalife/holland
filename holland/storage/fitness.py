a11cf642fc51962db41cd383b0b9b25662de7c26
henrywoody <github@henrywoody.com>
2018-10-03 17:57:42 -0700

    adds record_to_json method and main record method that calls record_to_csv or record_to_json; replaces record_to_csv with record in record_fitness

--- holland/storage/fitness.py
+++ holland/storage/fitness.py
@@ -1,14 +1,12 @@
 import statistics
 
-from .utils import record_to_csv
+from .utils import record
 
 
 def record_fitness(generation_num, fitness_scores, **storage_options):
     fitness_statistics = format_fitness_statistics(generation_num, fitness_scores)
 
-    storage_format = storage_options.get("format")
-    if storage_format == "csv":
-        record_to_csv(fitness_statistics, **storage_options)
+    record(fitness_statistics, **storage_options)
 
     return fitness_statistics
 
