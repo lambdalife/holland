ca51f6f616f0a5360b4a59dda5e1ee4c625926c4
henrywoody <github@henrywoody.com>
2018-10-03 17:57:42 -0700

    adds record_to_json method and main record method that calls record_to_csv or record_to_json; replaces record_to_csv with record in record_fitness

--- holland/storage/utils.py
+++ holland/storage/utils.py
@@ -1,4 +1,13 @@
 import os
+import json
+
+
+def record(data, **storage_options):
+    storage_format = storage_options.get("format")
+    if storage_format == "csv":
+        record_to_csv(data, **storage_options)
+    elif storage_format == "json":
+        record_to_json(data, **storage_options)
 
 
 def record_to_csv(data, **storage_options):
@@ -24,3 +33,18 @@ def record_to_csv(data, **storage_options):
     with open(full_path, "a") as f:
         f.write(",".join([str(v) for k, v in sorted_data]))
         f.write("\n")
+
+
+def record_to_json(data, **storage_options):
+    assert (
+        storage_options.get("file_name") is not None
+        and storage_options.get("path") is not None
+    )
+
+    file_name = storage_options["file_name"]
+    file_path = storage_options["path"]
+    full_path = os.path.join(file_path, file_name)
+
+    stringified_data = json.dumps(data)
+    with open(full_path, "w") as f:
+        f.write(stringified_data)
