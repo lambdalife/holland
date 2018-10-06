51057109cdc1893a1c4f79c957ea68c0c2858d50
henrywoody <github@henrywoody.com>
2018-10-06 14:10:59 -0700

    updates evaluation documentation

--- holland/evolution/evaluation.py
+++ holland/evolution/evaluation.py
@@ -1,13 +1,16 @@
 def evaluate_fitness(gene_pool, fitness_function):
     """
-    Evaluates the fitness of a population
+    Evaluates the fitness of a population by applying a fitness function to each genome in the population
 
-    :param gene_pool: a population to evaluate
+    :param gene_pool: a population of genomes to evaluate
     :type gene_pool: list
+
+    :param fitness_function: a function for evaluating the fitness of each genome; see :ref:`fitness-function`
+    :type fitness_function: func
+
 
     :returns: a list of tuples of the form ``(score, genome)``.
 
-	.. todo:: write an example
     """
 
     results = []
