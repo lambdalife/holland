1006c679906865b85ae952e4f550194d6db5db59
henrywoody <github@henrywoody.com>
2018-10-06 14:01:10 -0700

    updates crossover documentation and renames cross to cross_genomes

--- holland/evolution/crossover.py
+++ holland/evolution/crossover.py
@@ -1,26 +1,23 @@
-def cross(parent_genomes, genome_params):
+def cross_genomes(parent_genomes, genome_params):
     """
-    Crosses the genome of a given list of parents
+    Produces a new genome by applying crossover to multiple parent genomes
 
     .. note:: Holland generalizes the usual 2 parent crossover and supports k-parent crossover
 
-    :param parent_genomes: a list of parents
+    :param parent_genomes: a list of parent genomes
     :type parent_geomes: list
 
-    :param genome_params: a dictionary specifying genome parameters
+    :param genome_params: a dictionary specifying genome parameters, specifcally ``crossover_function`` is relevant; see :ref:`genome-params`
     :type genome_params: dict
 
 
-    :returns: a single genome based on the parent genomes
+    :returns: a single genome
 
-
-    Dependencies:
-        * :ref:`crossover-functions`
     """
     offspring = {}
     for gene_name in parent_genomes[0].keys():
-        crossover_function = genome_params[gene_name]["crossover_function"]
         parent_genes = [pg[gene_name] for pg in parent_genomes]
+        crossover_function = genome_params[gene_name]["crossover_function"]
         offspring[gene_name] = crossover_function(parent_genes)
 
     return offspring
