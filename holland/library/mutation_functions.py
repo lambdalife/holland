a781eb345685c0a9c45af64ece710e833c8c820f
henrywoody <github@henrywoody.com>
2018-10-07 19:15:46 -0700

    adds 'Valid For' field to documentation for mutation_functions

--- holland/library/mutation_functions.py
+++ holland/library/mutation_functions.py
@@ -4,6 +4,9 @@ import random
 def get_flip_mutation_function():
     """
     Returns a function that returns the negated value of the input, where the input is a boolean value; see :ref:`mutation-functions`
+    
+    :Valid For:
+        ``"bool"`` and ``"[bool]"`` gene types
 
 
     :returns: a function that returns the negated value if its input
@@ -36,6 +39,9 @@ def get_flip_mutation_function():
 def get_boundary_mutation_function(minimum, maximum):
     """
     Returns a function that pushes a value to either the minimum or maximum allowed value for a gene; see :ref:`mutation-functions`
+
+    :Valid For:
+        ``"int"``, ``"[int]"``, ``"float"``, and ``"[float]"`` gene types
 
     :param minimum: the minimum allowed value
     :type minimum: int/float
@@ -76,6 +82,9 @@ def get_uniform_mutation_function(minimum, maximum):
     """
     Returns a function that returns a value drawn from a uniform distribution over the closed interval [minimum, maximum]; see :ref:`mutation-functions`
 
+    :Valid For:
+        any gene type
+
     :param minimum: the minimum allowed value
     :type minimum: int/float
 
@@ -114,6 +123,9 @@ def get_uniform_mutation_function(minimum, maximum):
 def get_gaussian_mutation_function(sigma):
     """
     Returns a function that returns a value drawn from a gaussian (normal) distribution with mean equal to ``value`` and standard_deviation equal to ``sigma``
+
+    :Valid For:
+        ``"int"``, ``"[int]"``, ``"float"``, and ``"[float]"`` gene types
 
     :param sigma: standard deviation for the gaussian distribution
     :type sigma: int/float
