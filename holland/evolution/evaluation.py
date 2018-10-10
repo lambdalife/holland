a96f68a2b3b6c267f6125c0d89225d64da9f7540
henrywoody <github@henrywoody.com>
2018-10-10 13:54:11 -0700

    moves evaluation methods into Evaluator class and updates usage in evolve

--- holland/evolution/evaluation.py
+++ holland/evolution/evaluation.py
@@ -1,23 +1,30 @@
-def evaluate_fitness(gene_pool, fitness_function, ascending=True):
+class Evaluator:
     """
-    Evaluates the fitness of a population by applying a fitness function to each genome in the population
-
-    :param gene_pool: a population of genomes to evaluate
-    :type gene_pool: list
+    Handles evaluation of genomes
 
     :param fitness_function: a function for evaluating the fitness of each genome; see :ref:`fitness-function`
     :type fitness_function: func
 
     :param ascending: whether or not to sort results in ascending order of fitness
     :type ascending: bool
-
-
-    :returns: a sorted list of tuples of the form ``(score, genome)``.
-
     """
 
-    results = []
-    for genome in gene_pool:
-        score = fitness_function(genome)
-        results.append((score, genome))
-    return sorted(results, key=lambda x: x[0], reverse=(not ascending))
+    def __init__(self, fitness_function, ascending=True):
+        self.fitness_function = fitness_function
+        self.ascending = ascending
+
+    def evaluate_fitness(self, gene_pool):
+        """
+        Evaluates the fitness of a population by applying a fitness function to each genome in the population
+
+        :param gene_pool: a population of genomes to evaluate
+        :type gene_pool: list
+
+
+        :returns: a sorted list of tuples of the form ``(score, genome)``.
+        """
+        results = []
+        for genome in gene_pool:
+            score = self.fitness_function(genome)
+            results.append((score, genome))
+        return sorted(results, key=lambda x: x[0], reverse=(not self.ascending))
